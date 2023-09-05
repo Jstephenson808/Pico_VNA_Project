38dbf6204d3d3fb0978c8456039914009884f603
James Stephenson (work) <James.Stephenson@glasgow.ac.uk>
2023-09-05 11:52:34 +0100

    saving S21 to csv works, still in a spike phase, next is reading it back in

--- Project Files/save_S21_todf.py
+++ Project Files/save_S21_todf.py
@@ -10,9 +10,11 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 
+N_RUNS = 10
+MEASURE = 'S21'
 picoVNACOMObj = win32com.client.gencache.EnsureDispatch("PicoControl2.PicoVNA_2")
-df = pd.DataFrame(columns=['Time', 'S21 Data'])
-index = 0
+df = pd.DataFrame(columns=['Time', 'Frequency', 'Magnitude (dB)'])
+
 print("Connecting VNA")
 findVNA = picoVNACOMObj.FND()
 print('VNA ' + str(findVNA) + ' Loaded')
@@ -20,19 +22,29 @@ print('VNA ' + str(findVNA) + ' Loaded')
 print("Load Calibration")
 ans = picoVNACOMObj.LoadCal("C:\\Users\\js637s\\OneDrive - University of Glasgow\\Glasgow\\Summer Project\\VNA\\500MHz_3GHz_MiniCirc_P1Short_P2Long_m3dBm_Lab103_Mar23_200MHz_6GHz_.cal")
 print("Result " + str(ans))
+start_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-for i in range(50):
-    picoVNACOMObj.Measure('S21')
-    current_time = datetime.now()
+for index in range(N_RUNS):
+    picoVNACOMObj.Measure(MEASURE)
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    print("getting S21 LogMag Data")
-    raw = picoVNACOMObj.GetData("S21", "logmag", 0)
+
+    raw = picoVNACOMObj.GetData(MEASURE, "logmag", 0)
     split_data = raw.split(',')
     converted_data = np.array(split_data)
     converted_data = converted_data.astype(np.float)
+    frequency = converted_data[:: 2]
+    data = converted_data[1:: 2]
 
-    df = df.append({'Time': current_time, 'S21 Data': converted_data}, ignore_index=True)
-    index += 1
+    df = df.append({'Time': current_time, 'Frequency': frequency, 'Magnitude (dB)': data}, ignore_index=True)
 
     if index % 10 == 0:
-        df.to_csv(f'{}.csv', index=False)
+        print(f"Saving S21 LogMag Data Index is {index}")
+        df.to_csv(f'{MEASURE}_{N_RUNS}_Runs_{start_time}.csv', index=False)
+
+df.to_csv(f'{MEASURE}_{N_RUNS}_Runs_{start_time}.csv', index=False)
+
+a = picoVNACOMObj.CloseVNA()
+print("VNA Closed")
+
+
