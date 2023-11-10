82fdc52ef5dfcaeac46cf7c528f1587e788c4edc
James_Stephenson_Work <james.stephenson@glasgow.ac.uk>
2023-11-10 16:41:36 +0000

    VNA Class should work with new format and is an object

--- Project Files/VNA.py
+++ Project Files/VNA.py
@@ -12,7 +12,6 @@ from enum import Enum
 def mhz_to_hz(mhz):
     return mhz * 1000000
 
-
 class TakeMeasurement(Enum):
     S11 = "S11"
     S21 = "S21"
@@ -60,6 +59,8 @@ class VNA:
         self.vna_object = win32com.client.gencache.EnsureDispatch(vna_string)
         self.output_df_magnitude = pd.DataFrame(columns=[cols.value for cols in DataFrameCols])
 
+
+
     def connect(self):
         print("Connecting VNA")
         findVNA = self.vna_object.FND()
@@ -74,7 +75,8 @@ class VNA:
         return self.vna_object.GetData(parameter, data_format, point)
 
     def data_string_to_df(self,
-                          time: datetime, measurement_string: str,
+                          time: datetime,
+                          measurement_string: str,
                           parameter: GetMeasurement,
                           data_format: DataFrameCols) -> pandas.DataFrame:
         split_data = measurement_string.split(',')
@@ -89,16 +91,33 @@ class VNA:
         }
         return pd.DataFrame(data)
 
+    def generate_output_filename(self, fname: str, s_params_measure: TakeMeasurement, run_time: timedelta):
+        if fname != "":
+            fname += "_"
 
-    # could take in 
-    def measure(self, run_time: timedelta, s_params_measure: TakeMeasurement=TakeMeasurement
-                .ALL, output_s_param: [GetMeasurement]=[GetMeasurement.S21]):
+        return f'{fname}{s_params_measure}_{run_time.seconds}_secs.csv'
+
+
+    # could be its own class really
+    def measure(self,
+                run_time: timedelta,
+                s_params_measure: TakeMeasurement=TakeMeasurement.ALL,
+                output_s_param: [GetMeasurement]=[GetMeasurement.S21],
+                fname: str=""):
+
+        output_fname = self.generate_output_filename(fname, s_params_measure, run_time)
+        os.chdir("..")
+        os.chdir("VNA_Output")
+
         self.connect()
         self.load_cal()
+
+        output_data_frame = pd.DataFrame(columns=[col for col in DataFrameCols])
 
         start_time = datetime.now()
         finish_time = start_time + run_time
 
+        measurement_number = 0
         while datetime.now() < finish_time:
             elapsed_time = datetime.now() - start_time
             self.vna_object.Measure(s_params_measure)
@@ -106,14 +125,28 @@ class VNA:
                 magnitude_df = self.data_string_to_df(
                     elapsed_time,
                     self.get_data(s_param, MeasurementFormat.LOGMAG),
+                    s_param,
                     DataFrameCols.MAGNITUDE
-                )
+                    )
                 phase_df = self.data_string_to_df(
                     elapsed_time,
                     self.get_data(s_param, MeasurementFormat.PHASE),
+                    s_param,
                     DataFrameCols.PHASE
-                )
-                # append to df
+                    )
+                output_data_frame = pd.concat([output_data_frame, magnitude_df, phase_df])
+
+            measurement_number += 1
+            if measurement_number % 10 == 0:
+                print(f"Saving df data index is {measurement_number} running for another {(finish_time - datetime.now())}")
+                output_data_frame.to_csv(output_fname, index=False)
+
+        output_data_frame.to_csv(output_fname, index=False)
+
+        self.vna_object.CloseVNA()
+        print("VNA Closed")
+        return output_fname
+
 
 
 
