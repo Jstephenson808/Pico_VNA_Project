2be917886b7d49b9367d2ee089c51d00baa79db5
James Stephenson <jstephenson808@gmail.com>
2024-04-02 20:23:57 +0100

    lint

--- code/single_gesture_classifier.py
+++ code/single_gesture_classifier.py
@@ -1,16 +1,24 @@
 import pandas as pd
 
 from ml_model import *
-from VNA_utils import get_data_path, ghz_to_hz, mhz_to_hz, hz_to_ghz, get_frequency_column_headings_list
+from VNA_utils import (
+    get_data_path,
+    ghz_to_hz,
+    mhz_to_hz,
+    hz_to_ghz,
+    get_frequency_column_headings_list,
+)
+
 
 def extract_report_dictionary_from_test_results(result_dict):
     # need to just get the results
     columns = [x for x in result_dict.keys() if "report" in x]
-    return extract_gesture_metric_values(
-        result_dict, columns
-    )
+    return extract_gesture_metric_values(result_dict, columns)
 
-def test_data_frame_classifier_frequency_window_with_report(data_frame: pd.DataFrame, label:str, frequency_hop:int=mhz_to_hz(100))->pd.DataFrame:
+
+def test_data_frame_classifier_frequency_window_with_report(
+    data_frame: pd.DataFrame, label: str, frequency_hop: int = mhz_to_hz(100)
+) -> pd.DataFrame:
     movement_vector = create_movement_vector_for_single_data_frame(data_frame)
     freq_list = get_frequency_column_headings_list(data_frame)
     min_frequency, max_frequency = min(freq_list), max(freq_list)
@@ -24,51 +32,66 @@ def test_data_frame_classifier_frequency_window_with_report(data_frame: pd.DataF
         )
         fq_label = f"{label}_{hz_to_ghz(low_frequency)}_{hz_to_ghz(high_frequency)}"
         result, fname = feature_extract_test_filtered_data_frame(
-            data_frame_magnitude_filtered,
-            movement_vector,
-            fname=fq_label,
-            n_jobs=0
+            data_frame_magnitude_filtered, movement_vector, fname=fq_label, n_jobs=0
         )
         f1_scores[label] = extract_report_dictionary_from_test_results(result)
         low_frequency += frequency_hop
         high_frequency += frequency_hop
-    return pd.DataFrame.from_dict(f1_scores, orient="index", columns=[x for x in result.keys() if "report" in x])
+    return pd.DataFrame.from_dict(
+        f1_scores, orient="index", columns=[x for x in result.keys() if "report" in x]
+    )
 
-def test_classifier_from_df_dict(df_dict: {})->pd.DataFrame:
+
+def test_classifier_from_df_dict(df_dict: {}) -> pd.DataFrame:
 
     full_results_df = None
     for label, data_frame in df_dict.items():
-        result_df = test_data_frame_classifier_frequency_window_with_report(data_frame, label, frequency_hop=mhz_to_hz(100))
-        full_results_df = pd.concat(
-            (full_results_df, result_df))
+        result_df = test_data_frame_classifier_frequency_window_with_report(
+            data_frame, label, frequency_hop=mhz_to_hz(100)
+        )
+        full_results_df = pd.concat((full_results_df, result_df))
     return full_results_df
 
-def test_classifier_for_all_measured_params(combined_df: pd.DataFrame)->pd.DataFrame:
-    all_Sparams_magnitude = combined_df[
-        (combined_df["mag_or_phase"] == "magnitude")
-    ]
-    all_Sparams_phase = combined_df[
-        (combined_df["mag_or_phase"] == "phase")
-    ]
-    test_dfs = {f"{param.value}_magnitude": all_Sparams_magnitude[
-        all_Sparams_magnitude[DataFrameCols.S_PARAMETER.value] == param.value] for param in SParam}
+
+def test_classifier_for_all_measured_params(combined_df: pd.DataFrame) -> pd.DataFrame:
+    all_Sparams_magnitude = combined_df[(combined_df["mag_or_phase"] == "magnitude")]
+    all_Sparams_phase = combined_df[(combined_df["mag_or_phase"] == "phase")]
+    test_dfs = {
+        f"{param.value}_magnitude": all_Sparams_magnitude[
+            all_Sparams_magnitude[DataFrameCols.S_PARAMETER.value] == param.value
+        ]
+        for param in SParam
+    }
     test_dfs["all_Sparams_magnitude"] = all_Sparams_magnitude
     test_dfs["all_Sparams_phase"] = all_Sparams_phase
     test_dfs.update(
-        {f"{param.value}_phase": all_Sparams_phase[all_Sparams_phase[DataFrameCols.S_PARAMETER.value] == param.value]
-         for param in SParam})
+        {
+            f"{param.value}_phase": all_Sparams_phase[
+                all_Sparams_phase[DataFrameCols.S_PARAMETER.value] == param.value
+            ]
+            for param in SParam
+        }
+    )
     test_classifier_from_df_dict(test_dfs)
     return full_results_df
+
 
 if __name__ == "__main__":
     # results = open_pickled_object(os.path.join(get_pickle_path(), "classifier_results"))
     # stacked = results.stack()
     # combine dfs
     combined_df: pd.DataFrame = open_pickled_object(
-         os.path.join(get_pickle_path(), "full_dfs", os.listdir(os.path.join(get_pickle_path(), "full_dfs"))[0]))
-    #combined_df = combine_data_frames_from_csv_folder(
+        os.path.join(
+            get_pickle_path(),
+            "full_dfs",
+            os.listdir(os.path.join(get_pickle_path(), "full_dfs"))[0],
+        )
+    )
+    # combined_df = combine_data_frames_from_csv_folder(
     #     get_data_path(), label="single-watch-large-ant"
     # )
 
     full_results_df = test_classifier_for_all_measured_params(combined_df)
-    pickle_object(full_results_df, os.path.join(get_pickle_path(), "classifier_results"))
+    pickle_object(
+        full_results_df, os.path.join(get_pickle_path(), "classifier_results")
+    )
