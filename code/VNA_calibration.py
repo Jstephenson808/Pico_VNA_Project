588133a0230047e11623e0767b73d59abbf28ba5
James Stephenson <jstephenson808@gmail.com>
2024-06-11 16:41:09 +0100

    updated comments

--- code/VNA_calibration.py
+++ code/VNA_calibration.py
@@ -6,11 +6,19 @@ from VNA_exceptions import NotValidCalibrationFileException
 class VnaCalibration:
     """
     Holds data related to VNA calibration
+    provide a file path and the class will validate that this path is a .cal file for the pico VNA and
+    then extract out the npoints and fq sweep range automatically, throws an exception otherwise
 
     """
 
     @staticmethod
     def validate_line(line, pattern):
+        """
+        validates that the line matches the provided pattern
+        :param line: the line to test
+        :param pattern: regex pattern to match to
+        :return: None if successful
+        """
         if not re.match(pattern, line):
             raise NotValidCalibrationFileException(f"Not a valid calibration: {line}")
 
@@ -24,16 +32,24 @@ class VnaCalibration:
         self.low_freq_hz = None
         self.high_freq_hz = None
         self.fq_hop = None
-        self.verify_file_is_cal()
         self.extract_npoints_fq_range()
 
     def verify_file_is_cal(self):
+        """
+        tests provided file path for the correctly formatted .cal file
+        :return:
+        """
         sweep_plan_pattern = r'^(\d+,\d+,\d+,\d+,\d+,\d+)$'
         with open(self.calibration_path, 'r') as calibration_txt:
             lines = calibration_txt.readlines()
             self.validate_line(lines[2], sweep_plan_pattern)
 
     def extract_npoints_fq_range(self):
+        """
+        Extracts the npoints and fq range from the verified .cal file
+        :return: updates the class attrs with read values, throws exception if cal file is invalid
+        """
+        self.verify_file_is_cal()
         with open(self.calibration_path, 'r') as calibration_txt:
             lines = calibration_txt.readlines()
             sweep_plan_list = lines[2].strip().split(',')
