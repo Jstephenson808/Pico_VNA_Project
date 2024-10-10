46f0a6335e4fcbb2be1821029f3ea464d73ebc3c
james_work <james.stephenson@glasgow.ac.uk>
2024-10-10 15:58:11 +0100

    added some functions for confusion matrix, next is the s-param channels

--- code/graphs.py
+++ code/graphs.py
@@ -406,29 +406,56 @@ def gen_sweep_time_df(
 def make_confusion_matrix_dict_string_from_series(series: pd.Series)->str:
     return ('_').join([series['s_param'], series['type'], series['low_frequency'], series['high_frequency']])
 
+def confusion_matrix_from_single_result(single_result_series: pd.Series,
+                                        labels,
+                                        confusion_matrix_dict,
+                                        confusion_matrix_option: ConfusionMatrixKey)->None:
+
+    confusion_matrix_key = make_confusion_matrix_dict_string_from_series(single_result_series)
+    confusion_matrix = confusion_matrix_dict[confusion_matrix_key][confusion_matrix_option]
+    ConfusionMatrixDisplay(confusion_matrix, display_labels=labels).plot()
+    plt.title(
+        f'Confusion Matrix for {single_result_series["label"].split("_")[-1]} \n\r '
+        f'Using {single_result_series["classifier"].title()} classifier between {single_result_series["low_frequency"]} and {single_result_series["high_frequency"]} GHz')
+    plt.show()
+    return
+
+def display_confusion_matrix_for_top_value(full_df, results_df, confusion_matrix_dict):
+    full_df.columns = [pd.to_numeric(col, errors='coerce') if col.isnumeric() else col for col in full_df.columns]
+
+    accuracy_df = results_df[(results_df["gesture"] == "accuracy")]
+    accuracy_df = accuracy_df.sort_values(by='f1-score', ascending=False)
+    mag_df = accuracy_df[accuracy_df['type'] == 'magnitude']
+    top_magnitude = mag_df.iloc[0]
+
+
+    labels = [label[-1] for label in full_df['label'].unique()]
+    confusion_matrix_from_single_result(top_magnitude, labels, confusion_matrix_dict, confusion_matrix_option)
 
 
 if __name__ == "__main__":
     sns.set(rc={"xtick.bottom": True, "ytick.left": True}, font_scale=2)
-    full_df = open_pickled_object(r'C:\Users\2573758S\PycharmProjects\Pico_VNA_Project\pickles\full_dfs\17_09_patent_exp_combined_df.pkl')
+    pkl_classifier_folder = r'C:\Users\2573758S\PycharmProjects\Pico_VNA_Project\pickles\classifiers\smd_3_patent_exp'
+
+    full_df = open_pickled_object(r'C:\Users\js637s.CAMPUS\PycharmProjects\Pico_VNA_Project\pickles\full_dfs\full_combined_df_2024_08_09.pkl')
 
     confusion_matrix_option = ConfusionMatrixKey.FULL_SVM.value
-
-    full_df.columns = [pd.to_numeric(col, errors='coerce') if col.isnumeric() else col for col in full_df.columns]
-    results_df = get_full_results_df_from_classifier_pkls(r'C:\Users\2573758S\PycharmProjects\Pico_VNA_Project\pickles\classifiers\smd_3_patent_exp')
+    results_df = get_full_results_df_from_classifier_pkls(pkl_classifier_folder
+                                                          )
     accuracy_df = results_df[(results_df["gesture"] == "accuracy")]
     accuracy_df = accuracy_df.sort_values(by='f1-score', ascending=False)
     mag_df = accuracy_df[accuracy_df['type'] == 'magnitude']
     top_magnitude = mag_df.iloc[0]
     filtered_df_s_param = full_df[full_df['s_parameter'].isin(top_magnitude['s_param'].split('_'))]
-    filtered_df_fq_range = filter_cols_between_fq_range(filtered_df_s_param, ghz_to_hz(float(top_magnitude['low_frequency'])), ghz_to_hz(float(top_magnitude['high_frequency'])))
-    confusion_matrix_dict = get_full_results_df_from_classifier_pkls(r'C:\Users\2573758S\PycharmProjects\Pico_VNA_Project\pickles\classifiers\smd_3_patent_exp', extract="confusion_matrix")
-    confusion_matrix_key = make_confusion_matrix_dict_string_from_series(top_magnitude)
-    confusion_matrix = confusion_matrix_dict[confusion_matrix_key][confusion_matrix_option]
-    labels = [label[-1] for label in full_df['label'].unique()]
-    ConfusionMatrixDisplay(confusion_matrix, display_labels=labels).plot()
-    plt.title(f'Confusion Matrix for {top_magnitude["label"].split('_')[-1]} \n\r Using {top_magnitude["classifier"].title()} classifier between {top_magnitude['low_frequency']} and {top_magnitude['high_frequency']} GHz')
-    plt.show()
+
+    filtered_df_fq_range = filter_cols_between_fq_range(filtered_df_s_param,
+                                                        ghz_to_hz(float(top_magnitude['low_frequency'])),
+                                                        ghz_to_hz(float(top_magnitude['high_frequency'])))
+    confusion_matrix_dict = get_full_results_df_from_classifier_pkls(
+        pkl_classifier_folder,
+        extract="confusion_matrix")
+
+
     #pickle_object(results_df, path=r'C:\Users\2573758S\PycharmProjects\Pico_VNA_Project\pickles\full_classification_results', file_name='smd_3_patent_exp.pkl')
     #
     # #
