6a5e9af5e8155b191f07753759c0e100226ff385
James Stephenson (work) <2573758s@student.gla.ac.uk>
2024-12-12 15:55:25 +0000

    update to scipiInteraction.py

--- code/scipiInteraction.py
+++ code/scipiInteraction.py
@@ -1,3 +1,4 @@
+import os.path
 from time import sleep
 
 import pyvisa
@@ -24,14 +25,15 @@ VNA_VISA_ADDRESS = "USB0::0xF4EC::0x1700::SNA5XCED5R0097::INSTR"
 
 
 RUN_TIME_DELTA = timedelta(seconds=5)
-COUNTDOWN_TIME_SECONDS = 10
+COUNTDOWN_TIME_SECONDS = 1
 TEST_NAME = f"auto_test"
 SAVE_ROOT = (
     f"local/James/Live_Captures/{datetime.now().strftime('%y%m%d%H%M')}_{TEST_NAME}"
 )
 snp = SnP.S4P
-n_tests = 25
-test_gestures = ["A", "B", "C", "1", "2", "3"]
+n_tests = 2
+test_gestures = ["1"]
+# test_gestures = ["A", "B", "C", "1", "2", "3"]
 
 rm = pyvisa.ResourceManager(NI_VISA_DLL_PATH)
 
@@ -40,7 +42,10 @@ print(f'Connected to {SNA.query("*IDN?")}')
 
 SNA.write(set_snp_save_ports_command_string(snp))
 
+
 # create dir -> unsure what happens if dir already exists? -> may be quite unsafe
+print(os.path.dirname(SAVE_ROOT))
+SNA.write(create_directory_command_string(os.path.dirname(SAVE_ROOT)))
 SNA.write(create_directory_command_string(SAVE_ROOT))
 
 # how can I get n points, freq range from the VNA?
@@ -48,14 +53,14 @@ SNA.write(create_directory_command_string(SAVE_ROOT))
 for gesture in test_gestures:
     print(f"Gesture: {gesture}")
     input("Press enter to continue...")
-    current_gesture_folder = f"{SAVE_ROOT}/{gesture}"
+    current_gesture_folder = f"{SAVE_ROOT}/Gesture_{gesture}"
     SNA.write(create_directory_command_string(current_gesture_folder))
-    for test in range(n_tests):
+    for test_number in range(1, n_tests + 1):
         # create unique folder for test
-        current_test_foler = f"{current_gesture_folder}/{test}"
+        current_test_foler = f"{current_gesture_folder}/{test_number}"
         SNA.write(create_directory_command_string(current_test_foler))
 
-        print(f"Test {test+1}")
+        print(f"Test {test_number}")
         countdown_timer(COUNTDOWN_TIME_SECONDS)
         run_time = RUN_TIME_DELTA
         start_time = datetime.now()
@@ -74,7 +79,11 @@ for gesture in test_gestures:
             if i % 100 == 0:
                 elapsed_time = current_time - start_time
                 print(f"Running for another {(run_time - elapsed_time)} i={i}")
-            SNA.write(save_snp_command_string(f"{current_test_foler}/capture_{i}"))
+            SNA.write(
+                save_snp_command_string(
+                    f"{current_test_foler}/capture_{str(i).zfill(5)}", SnP.S4P
+                )
+            )
             current_time = datetime.now()
             i += 1
 
