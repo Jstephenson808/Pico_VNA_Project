2540643a70815ebb9e7c195ba7977697995c7857
James Stephenson (work) <2573758s@student.gla.ac.uk>
2025-01-03 12:02:52 +0000

    formatting

--- vna/classifier.py
+++ vna/classifier.py
@@ -7,6 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 from vna.feature_extractor import ExtractedFeatures
 from vna.movement_vector import MovementVector
 
+
 class ClassificationResults:
     def __init__(self):
         self.classification_test_results = None
@@ -15,8 +16,11 @@ class ClassificationResults:
 
     def process_test_results(self, *, test_results, test_labels):
         self.classification_test_results = test_results
-        self.classification_test_report = classification_report(test_labels, test_results, output_dict=True)
+        self.classification_test_report = classification_report(
+            test_labels, test_results, output_dict=True
+        )
         self.confusion_matrix = confusion_matrix(test_labels, test_results)
+
 
 class Classifier(ABC):
 
@@ -29,7 +33,14 @@ class PicoDecisionTreeClassifier(Classifier):
     """
     By passing in the classifier object the parameters of the classification can be tweaked
     """
-    def __init__(self, *, classifier: DecisionTreeClassifier = DecisionTreeClassifier(), full_data_set: ExtractedFeatures, movement_vector: MovementVector):
+
+    def __init__(
+        self,
+        *,
+        classifier: DecisionTreeClassifier = DecisionTreeClassifier(),
+        full_data_set: ExtractedFeatures,
+        movement_vector: MovementVector
+    ):
         self.classifier = classifier
         self.training_data = full_data_set
         self.movement_vector = movement_vector
@@ -37,12 +48,13 @@ class PicoDecisionTreeClassifier(Classifier):
 
     def run_classifier(self):
         training_data, test_data, training_labels, test_labels = train_test_split(
-            self.training_data.extracted_features, self.movement_vector.movement_vector, test_size=0.4
+            self.training_data.extracted_features,
+            self.movement_vector.movement_vector,
+            test_size=0.4,
         )
 
         self.classifier = self.classifier.fit(training_data, training_labels)
         test_results = self.classifier.predict(test_data, test_labels)
-        self.classification_results.process_test_results(test_results=test_results, test_labels=test_labels)
-
-
-
+        self.classification_results.process_test_results(
+            test_results=test_results, test_labels=test_labels
+        )
