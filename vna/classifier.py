1ff2ea5c2825e51b4c226438ce3757d49da3a604
James Stephenson (work) <2573758s@student.gla.ac.uk>
2025-01-21 15:30:55 +0000

    added ABC for classifier to provide interface class

--- vna/classifier.py
+++ vna/classifier.py
@@ -2,13 +2,26 @@ from abc import ABC, abstractmethod
 
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 from vna.feature_extractor import ExtractedFeatures
 from vna.movement_vector import MovementVector
 
 
-class ClassificationResults:
+class ClassificationResults(ABC):
+
+    @abstractmethod
+    def process_test_results(self, *, test_results, test_labels):
+        pass
+
+
+class SkLearnClassificationResults:
+    """
+    Holds the results of the classification, the report generated as well as confusion matrix
+    """
+
     def __init__(self):
         self.classification_test_results = None
         self.classification_test_report = None
@@ -37,16 +50,51 @@ class PicoDecisionTreeClassifier(Classifier):
     def __init__(
         self,
         *,
-        classifier: DecisionTreeClassifier = DecisionTreeClassifier(),
+        decision_tree_classifier: DecisionTreeClassifier = DecisionTreeClassifier(),
         full_data_set: ExtractedFeatures,
         movement_vector: MovementVector
     ):
-        self.classifier = classifier
+        self.classifier = decision_tree_classifier
         self.training_data = full_data_set
         self.movement_vector = movement_vector
-        self.classification_results: ClassificationResults = ClassificationResults()
+        self.classification_results: SkLearnClassificationResults = (
+            SkLearnClassificationResults()
+        )
 
     def run_classifier(self):
+        training_data, test_data, training_labels, test_labels = train_test_split(
+            self.training_data.extracted_features,
+            self.movement_vector.movement_vector,
+            test_size=0.4,
+        )
+
+        self.classifier = self.classifier.fit(training_data, training_labels)
+        test_results = self.classifier.predict(test_data, test_labels)
+        self.classification_results.process_test_results(
+            test_results=test_results, test_labels=test_labels
+        )
+
+
+class SupportVectorClassifier(Classifier):
+    def __init__(
+        self,
+        *,
+        svc_classifier: SVC = SVC(),
+        full_data_set: ExtractedFeatures,
+        movement_vector: MovementVector,
+        scaler: StandardScaler = StandardScaler()
+    ):
+        self.classifier = svc_classifier
+        self.training_data = full_data_set
+        self.movement_vector = movement_vector
+        self.classification_results: SkLearnClassificationResults = (
+            SkLearnClassificationResults()
+        )
+        self.scaler = scaler
+        self.data_transformed_flag = False
+
+    def run_classifier(self):
+
         training_data, test_data, training_labels, test_labels = train_test_split(
             self.training_data.extracted_features,
             self.movement_vector.movement_vector,
